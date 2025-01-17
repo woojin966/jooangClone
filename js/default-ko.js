d0ae132c7ae425f5c8ed4779adc50a4fc181ee88
yejikim <yejikim1112@groundk.com>
2025-01-17 12:14:44 +0900

    재클론시작

--- js/default-ko.js
+++ js/default-ko.js
@@ -1,101 +1,28 @@
-// skrollr.init({});
+// 네임스페이스 : 이벤트 이름에 추가적인 식별자를 붙여서(이벤트를 변수에 저장함) 이벤트를 구분하고 충돌과 중복을 방지하기 위함. nsScrollEnd가 네임스페이스인데, 같은 scroll이벤트가 여러군데에서 발생하더라도 서로 충돌하지 않고 독립적으로 처리하기 위함
 
-$(document).ready(function() {
-  $(window).on('scroll', function() {
-    // 현재 스크롤 위치
-    var scrollTop = $(this).scrollTop();
+// ScrollEnd
+var ignoreScroll = false; // ignoreScroll 전역변수로 초기값 설정
+var ns = (new Date).getTime(); // 현재 시간을 밀리초 단위로 변환해 네입스페이스(nsScrollEnd)에 고유성을 부여
+var special = $.event.special; // $.event.special을 통해 jQuery에서 제공하는 특수 이벤트 기능에 접근
+var dispatch = $.event.handle || $.event.dispatch; // jQuery이벤트 핸들러를 실행하는 기본 함수
+var scroll = 'scroll'; // 기본적 스크롤 이벤트
+var scrollEnd = scroll + 'end'; // 기본적 스클로 이벤트를 감지한 후 발생할 커스텀 이벤트
+var nsScrollEnd = scroll + '.' + scrollEnd + ns // 고유 네임스페이스를 추가하여 scrollEnd이벤트가 다른 이벤트들과 충돌하지 않도록 함
+special.scrollend = {
+  delay : 300, // 스크롤 종료를 감지하기 위한 지연시간 : 스크롤이 끝났다고 판단할 시간 - 일정시간(300ms)동안 스크롤 이벤트가 발생하지 않으면 scrollEnd 이벤트가 발생하도록 함
+  setup : function(){ // scrollEnd 이벤틀르 사용할 준비
+    var pid, handler = function(e){ // pid : 타이머 ID. 이전에 설정된 타이머를 취소하기 위해 사용, handler : 실제 스크롤을 처리하는 함수. 스크롤 이벤트가 발생할 때마다 호출됨
+      var _this = this, args = arguments;
 
-    // 스크롤 위치가 20px 이상이고 40px 이하일 때
-    if (scrollTop >= 20 && scrollTop <= 80) {
-      // 스크롤 위치에 따라 width 계산 (0%에서 100%로 변화)
-      var widthPercentage = ((scrollTop - 20) / 20) * 100; // 20px에서 40px까지 변화
-
-      // .thumb의 width를 설정
-      $('.visualSlideInner .thumb').css({
-        'width': widthPercentage + '%'
-      });
-    } else if (scrollTop < 20) {
-      // 스크롤이 20px 미만일 때는 width 0%
-      $('.visualSlideInner .thumb').css({
-        'width': '20%',
-        'height' : '40dvh',
-        'transition' : '1s',
-      });
-      $('.visualSlideBox .keyText, .visualSlideInner .beforeBox').css({
-        'visibility' : 'visible',
-        'opacity' : 1,
-        'padding-left' : 0,
-        'transition' : '1s',
-      });
-      $('section.keyVisual').css({
-        'padding': '200px 0 0 24%',
-        'transition' : '1s',
-      });
-    } else if (scrollTop > 80) {
-      // 스크롤이 40px 이상일 때는 width 100%
-      $('.visualSlideInner .thumb').css({
-        'width': '100%',
-        'height' : '100dvh',
-        'transition' : '1s',
-      });
-      $('.visualSlideBox .keyText, .visualSlideInner .beforeBox').css({
-        'visibility' : 'hidden',
-        'opacity' : 0,
-        'padding-left' : '24%',
-        'transition' : '1s',
-      });
-      $('section.keyVisual').css({
-        'padding': '200px 0 0',
-        'transition' : '1s',
-      });
-      $('section').addClass('on');
-      $('section.keyVisual.on').removeClass('on').css({
-        'height' : 0,
-        'padding' : 0,
-        'transition' : '1s',
-      });
-    }
-
-
-    var lastScrollTop = 0; // 마지막 스크롤 위치
-    var sections = $('.section.cont'); // 모든 섹션을 선택
-    var scrollTop = $(this).scrollTop(); // 현재 스크롤 위치
-
-    // 섹션들을 하나씩 확인하여 스크롤 위치에 맞는 섹션만 visible 클래스를 추가
-    sections.each(function(index) {
-      var sectionTop = $(this).offset().top; // 섹션의 상단 위치
-      var sectionHeight = $(this).outerHeight(); // 섹션의 높이
-
-      // 스크롤 위치가 섹션에 도달했을 때 visible 클래스를 추가
-      if (scrollTop > sectionTop - $(window).height() && scrollTop < sectionTop + sectionHeight) {
-        // 기존에 visible 클래스를 가진 섹션들에서 클래스를 제거
-        sections.removeClass('visible');
-        // 현재 섹션에만 visible 클래스를 추가
-        $(this).addClass('visible');
-      } else {
-        $(this).removeClass('visible'); // 스크롤이 섹션을 지나가면 visible 클래스를 제거
-      }
-    });
-
-
-    const scrollPosition = $(window).scrollTop(); // 현재 스크롤 위치
-    const footerTop = $('#footer').offset().top; // #footer의 상단 위치
-    const windowHeight = $(window).height(); // 브라우저 창 높이
-
-    // #footer가 화면에 보이기 시작했을 때
-    if (scrollPosition + windowHeight > footerTop) {
-      // .utilWrap에 'hidden' 클래스 추가
-      $('.utilWrap').addClass('hidden');
-    } else {
-      // #footer가 화면에 보이지 않으면 'hidden' 클래스 제거
-      $('.utilWrap').removeClass('hidden');
-    }
-  });
-
-  // const swiper = new Swiper(".sectionWrap", {
-  //   direction: "vertical",
-  //   slidesPerView: 1,
-  //   spaceBetween: 30,
-  //   mousewheel: true,
-  // });
-});
+      clearTimeout(pid); // 이전에 설정된 타임아웃 취소, 스크롤이 계속해서 발생하면 scrollEnd 이벤트가 중복으로 발생하지 않도록 함
+      pid = setTimeout(function(){ // 300ms 동안 추가적인 스크롤이 없으면 scrollEnd 트리거
+        e.tpye = scrollEnd; // 이벤트 타입을 'scrollEnd로 변경'
+        dispatch.apply(_this, args); // 커스텀 이벤트를 호출 : scrollEnd이벤트를 실제로 발생시킴
+      }, special.scrollend.delay); // 지연시간 후 이벤트 발생
+    };
+    $(this).on(nsScrollEnd, handler); // 해당 DOM 요소에 대해 scrollEnd 이벤트를 설정
+  },
+  teardown : function(){ // scrollEnd이벤트를 더이상 사용하지 않을때 이벤트 제거
+    $(this).off(nsScrollEnd); // 해당 DOM요소에 scrollEnd 이벤트 핸들러를 제거
+  }
+};
