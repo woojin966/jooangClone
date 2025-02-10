cb7c1712f9901f467022d4e7922c33f4ba18813a
yejikim <yejikim1112@groundk.com>
2025-02-10 14:37:05 +0900

    wip

--- js/default-ko.js
+++ js/default-ko.js
@@ -11,8 +11,15 @@ $(document).ready(function() {
         // clip-path 값을 계산
         var insetTopBottom = (35 - (35 * scrollRatio)) + "%"; // 35에서 0까지 감소
         var insetLeftRight = (24 - (24 * scrollRatio)) + "%"; // 0에서 24까지 증가
+        var layerGap = (35 - (35 * scrollRatio) + "%");
 
         // 새로운 clip-path 값 적용
         $('.sec1').css('clip-path', 'inset(' + insetTopBottom + ' ' + insetLeftRight + ' ' + insetTopBottom + ' ' + insetLeftRight + ')');
+        $('.layer').css('gap', layerGap + '%');
+        if(scrollRatio == 1){
+            $('.layer').css('opacity', '0');
+        } else {
+            $('.layer').css('opacity', '1');
+        }
     });
 });
