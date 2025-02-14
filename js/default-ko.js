e6707a69725bf0869bb81cd19a9894de8be095f3
yejikim <yejikim1112@groundk.com>
2025-02-14 11:11:21 +0900

    wip

--- js/default-ko.js
+++ js/default-ko.js
@@ -21,9 +21,32 @@ $(document).ready(function() {
         var scrollRatio = Math.min(scrollPosition / sec1Height, 1);
 
         // clip-path 값을 계산
-        var insetTopBottom = (35 - (35 * scrollRatio)) + "%"; // 35에서 0까지 감소
-        var insetLeftRight = (24 - (24 * scrollRatio)) + "%"; // 0에서 24까지 증가
+        var insetTop = (35 - (35 * scrollRatio)) + "%";
+        var insetBottom = (35 - (35 * scrollRatio)) + "%";
+        var insetLeft = (24 - (24 * scrollRatio)) + "%";
+        var insetRight = (21 - (21 * scrollRatio)) + "%";
         var layerGap = (35 - (35 * scrollRatio)) + "%";
+        if($(window).width() <= 1440){
+            var insetTop = (36 - (36 * scrollRatio)) + "%";
+            var insetBottom = (36 - (36 * scrollRatio)) + "%";
+            var insetLeft = (24 - (24 * scrollRatio)) + "%";
+            var insetRight = (20 - (20 * scrollRatio)) + "%";
+            var layerGap = (32 - (32 * scrollRatio)) + "%";
+        }
+        if($(window).width() <= 1024){
+            var insetTop = (38 - (38 * scrollRatio)) + "%";
+            var insetBottom = (38 - (38 * scrollRatio)) + "%";
+            var insetLeft = (16 - (16 * scrollRatio)) + "%";
+            var insetRight = (12 - (12 * scrollRatio)) + "%";
+            var layerGap = (28 - (28 * scrollRatio)) + "%";
+        }
+        if($(window).width() <= 840){
+            var insetTop = (40 - (40 * scrollRatio)) + "%";
+            var insetBottom = (40 - (40 * scrollRatio)) + "%";
+            var insetLeft = (0 - (0 * scrollRatio)) + "%";
+            var insetRight = (0 - (0 * scrollRatio)) + "%";
+            var layerGap = (28 - (28 * scrollRatio)) + "%";
+        }
 
         if (scrollRatio == 1) {
             $('.layer').css({'position' : 'absolute', 'visibility' : 'hidden', 'opacity': '0', 'color': '#fff'}); // opacity 0, color #fff
@@ -33,19 +56,33 @@ $(document).ready(function() {
             $('.layer').css({'position' : 'fixed', 'visibility' : 'visible', 'opacity': '1', 'color': color}); // opacity 1, color 서서히 변화
         }
         // 새로운 clip-path 값 적용
-        $('.sec1').css('clip-path', 'inset(' + insetTopBottom + ' ' + insetLeftRight + ' ' + insetTopBottom + ' ' + insetLeftRight + ')');
+        $('.sec1').css('clip-path', 'inset(' + insetTop + ' ' + insetRight + ' ' + insetBottom + ' ' + insetLeft + ')');
         $('.layer').css({'gap' : layerGap});
 
         // footer 상단에 도달하면 .utilWrap을 왼쪽으로 숨기기
         if (scrollPosition + $(window).height() >= footerOffset) {
             $('.utilWrap').css({'left' : '-9999px', 'transition' : 'all 2s ease-in-out'}); // .utilWrap을 왼쪽으로 숨기기
         } else {
-            $('.utilWrap').css({'left' : '-44px', 'transition' : 'all 1s ease-in-out'}); // .utilWrap을 원래 위치로 복귀
+            $('.utilWrap').css({'left' : '-2.292vw', 'transition' : 'all 1s ease-in-out'}); // .utilWrap을 원래 위치로 복귀
+            if($(window).width() <= 1440){
+                $('.utilWrap').css({'left' : '-4.167vw', 'transition' : 'all 1s ease-in-out'});
+            }
+            if($(window).width() <= 1024){
+            }
         }
     });
 
-    document.querySelector('#intro').addEventListener('loadedmetadata', function(){
-        this.currentTime = 3;
-        this.play();
+    // document.querySelector('#intro').addEventListener('loadedmetadata', function(){
+    //     this.currentTime = 3;
+    //     this.play();
+    // });
+
+    document.querySelector('#intro').addEventListener("play", function() {
+        setTimeout(function() {
+            document.querySelector('#intro').pause();
+        }, 6000); // 4초 후에 일시정지
     });
+
+    // 비디오가 로드될 때 자동으로 재생 시작
+    document.querySelector('#intro').play();
 });
