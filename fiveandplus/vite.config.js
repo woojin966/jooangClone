ede7d016ecb9bd68633e10c63e863d83bd7ff8fc
yejikim <yejikim1112@groundk.com>
2025-02-24 17:29:18 +0900

    vue호ㅏ!1

---
+++ fiveandplus/vite.config.js
@@ -0,0 +1,26 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueDevTools from 'vite-plugin-vue-devtools'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    vueDevTools(),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    },
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        // 글로벌 SCSS 변수나 mixin 등을 추가할 수 있습니다.
+        //additionalData: '@use "@/assets/landing.scss";' // 예시로 글로벌 변수 파일을 추가
+      }
+    }
+  }
+})
