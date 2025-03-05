ff2ab9a80d1621f010bc0364dfb7268cd5233cb5
woojin <77970494+woojin966@users.noreply.github.com>
2025-03-05 22:58:37 +0900

    컴포넌트화

--- fiveandplus/vite.config.js
+++ fiveandplus/vite.config.js
@@ -10,7 +10,6 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
-  base : '/fiveandplus',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
